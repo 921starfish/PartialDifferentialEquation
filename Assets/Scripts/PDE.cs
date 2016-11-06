e1b83400781433e57a2ed8924e0a6bbfa7374cd5
921starfish <yuya.921.hoshino@gmail.com>
2016-11-07 03:12:06 +0900

    gridview追加の準備

--- Assets/Scripts/PDE.cs
+++ Assets/Scripts/PDE.cs
@@ -5,9 +5,13 @@ public class PDE : MonoBehaviour
 {
 
     static double[,,] log;
-    static bool isDiscrete = true;
-    int count;
-    double X, Y, W, H;
+    public bool isDiscrete = false;
+    int count, height, width;
+    float X, Y, W, H;
+    public GameObject Rectangle;
+    public GameObject[,] Rects;
+    [Range(0, 50)]
+    public int Time = 0;
 
     // Use this for initialization
     void Start()
@@ -18,8 +22,8 @@ public class PDE : MonoBehaviour
         double kappa = 0.0023473150395; // 鉄の熱伝達係数 m^2/s
         double lambda = kappa * dt / dx / dx;
         Debug.Log(" alpha = " + lambda);
-        int height = (int)(0.6 / dx) + 1; // 格子点の数 (長さ60cm)
-        int width = (int)(0.4 / dx) + 1; // 格子点の数 (幅40cm)
+        height = (int)(0.6 / dx) + 1; // 格子点の数 (長さ60cm)
+        width = (int)(0.4 / dx) + 1; // 格子点の数 (幅40cm)
         int m = 50000; // 時間格子の数
         double[,] T = new double[height, width];
         double[,] TT = new double[height, width];
@@ -108,11 +112,113 @@ public class PDE : MonoBehaviour
                 }
             }
         }
+        makeGraph();
+        setColorToGraph(0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        setColorToGraph(Time);
+    }
 
+    // グラフ描画関数
+    public void makeGraph()
+    {
+        X = 0;
+        Y = 0;
+        W = 410;
+        H = 610;
+
+        Rects = new GameObject[height, width];
+
+        float h = (H - Y) / log.GetLength(1);
+        float w = (W - X) / log.GetLength(2);
+
+        for (int j = 0; j < log.GetLength(1); j++)
+        {
+            for (int k = 0; k < log.GetLength(2); k++)
+            {
+                Rects[j, k] = Instantiate(Rectangle);
+                Rects[j, k].transform.localScale = new Vector3(h, w, 0);
+                Rects[j, k].transform.position += new Vector3((float)(X + w * k), (float)(Y + h * j), 0);
+            }
+        }
+    }
+
+    // グラフに色を付ける関数
+    public void setColorToGraph(int i)
+    {
+        for (int j = 0; j < log.GetLength(1); j++)
+        {
+            for (int k = 0; k < log.GetLength(2); k++)
+            {
+                Rects[j, k].GetComponent<SpriteRenderer>().color = makeColor(log[i, j, k]);
+            }
+        }
+    }
+
+    // 温度を色に変える関数
+    public Color makeColor(double t)
+    {
+        if (!isDiscrete)
+        {
+            return Color.HSVToRGB((float)(1 - t / 500) * (5f / 9f), 1.0f, 1.0f);
+        }
+
+        if (0 < t && t <= 50)
+        {
+            return Color.HSVToRGB((float)(1 - 50f / 500) * (5f / 9f), 1.0f, 1.0f);
+            // return new Color32(186, 0, 255, 255);
+        }
+        else if (50 < t && t <= 100)
+        {
+            return Color.HSVToRGB((float)(1 - 100f / 500) * (5f / 9f), 1.0f, 1.0f);
+            // return new Color32(138, 43, 226, 255);
+        }
+        else if (100 < t && t <= 150)
+        {
+            return Color.HSVToRGB((float)(1 - 150f / 500) * (5f / 9f), 1.0f, 1.0f);
+            // return Color.blue;
+        }
+        else if (150 < t && t <= 200)
+        {
+            return Color.HSVToRGB((float)(1 - 200f / 500) * (5f / 9f), 1.0f, 1.0f);
+            // return Color.cyan;
+        }
+        else if (200 < t && t <= 250)
+        {
+            return Color.HSVToRGB((float)(1 - 250f / 500) * (5f / 9f), 1.0f, 1.0f);
+            // return new Color32(0, 255, 0, 255);
+        }
+        else if (250 < t && t <= 300)
+        {
+            return Color.HSVToRGB((float)(1 - 300f / 500) * (5f / 9f), 1.0f, 1.0f);
+            // return new Color32(124, 252, 0, 255);
+        }
+        else if (300 < t && t <= 350)
+        {
+            return Color.HSVToRGB((float)(1 - 350f / 500) * (5f / 9f), 1.0f, 1.0f);
+            // return new Color32(173, 255, 47, 255);
+        }
+        else if (350 < t && t <= 400)
+        {
+            return Color.HSVToRGB((float)(1 - 400f / 500) * (5f / 9f), 1.0f, 1.0f);
+            // return Color.yellow;
+        }
+        else if (400 < t && t <= 450)
+        {
+            return Color.HSVToRGB((float)(1 - 450f / 500) * (5f / 9f), 1.0f, 1.0f);
+            // return new Color32(255, 165, 0, 255);
+        }
+        else if (450 < t && t <= 500)
+        {
+            return Color.HSVToRGB((float)(1 - 500f / 500) * (5f / 9f), 1.0f, 1.0f);
+            // return Color.red;
+        }
+        else
+        {
+            return Color.clear;
+        }
     }
 }
---
+++ Assets/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+    public GameObject rectangle;
+
+    // Use this for initialization
+    void Start()
+    {
+        Instantiate(rectangle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
